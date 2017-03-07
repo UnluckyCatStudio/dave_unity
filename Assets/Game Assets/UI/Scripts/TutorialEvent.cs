c03d4f12d0d84aa73ffb038055f3bd38f7fb2555
Vic <resource.v101@gmail.com>
2017-03-07 05:54:29 +0100

    tutorial y bueno nse la vd

---
+++ Assets/Game Assets/UI/Scripts/TutorialEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Kyru.UI;
+
+public class TutorialEvent : MonoBehaviour
+{
+	// Localization key
+	public string key;
+
+	public void Trigger ()
+	{
+		var t = GameObject.Find ( "TXT_Tuto" ).GetComponent<Text> ();
+		t.text = Localization.texts[key];
+
+		Time.timeScale = 0;
+		Game.ui.SetTrigger ( "Tutorial" );
+		Destroy ( this );
+	}
+}
