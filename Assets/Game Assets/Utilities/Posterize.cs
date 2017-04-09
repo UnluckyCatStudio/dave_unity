16bebc7875c9caa7a5b1ef4bf3d19ebe74dc4b87
Vic <resource.v101@gmail.com>
2017-04-09 18:03:22 +0200

    cosis

---
+++ Assets/Game Assets/Utilities/Posterize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.ImageEffects;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[AddComponentMenu ("Image Effects/Posterize")]
+public class Posterize : ImageEffectBase
+{
+	[Range (1f, 64f)]	public float amountOfColors;
+	[Range (0.01f, 2f)] public float gammaValue;
+
+	void OnRenderImage ( RenderTexture source, RenderTexture destination )
+	{
+		material.SetFloat ( "_Colors", amountOfColors );
+		material.SetFloat ( "_Gamma", gammaValue );
+		Graphics.Blit ( source, destination, material );
+	}
+}
