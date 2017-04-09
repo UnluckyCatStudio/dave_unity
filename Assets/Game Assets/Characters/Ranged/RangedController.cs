38b813898c414023c0fd0d0f429466157b93c8e4
Anna <tropiicanna@gmail.com>
2017-04-10 00:13:07 +0200

    vfx

---
+++ Assets/Game Assets/Characters/Ranged/RangedController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedController : MonoBehaviour {
+
+	public ParticleSystem ranged;
+	public ParticleSystem death;
+	public ParticleSystem shoot;
+	public ParticleSystem decoration;
+	public ParticleSystem charge;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Game Assets/VFX/FireController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour {
+
+	public ParticleSystem fire;
+	public ParticleSystem smoke;
+	public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
