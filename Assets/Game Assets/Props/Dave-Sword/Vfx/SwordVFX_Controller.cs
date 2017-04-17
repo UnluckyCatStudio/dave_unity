738adc9ecf7a6e8b5a8f5e2872c3ce34e9312865
Anna <tropiicanna@gmail.com>
2017-04-17 15:03:29 +0200

    hud + placeta
    prefab de la placeta meh
    prefab del hud okay

---
+++ Assets/Game Assets/Props/Dave-Sword/Vfx/SwordVFX_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordVFX_Controller : MonoBehaviour {
+
+	[Header ("SWORD HUD")]
+	[Space (5)]
+	public ParticleSystem HUD;
+	public ParticleSystem Death;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
